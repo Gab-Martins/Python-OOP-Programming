7bde4e28e25963c9fc1cfd16bf2a2a6119e7f4aa
Gab-Martins <132864619+Gab-Martins@users.noreply.github.com>
2024-01-28 23:08:56 +0000

    corrected main functions

--- financial-calculator.py
+++ financial-calculator.py
@@ -3,28 +3,54 @@ class Employee:
         self.name = input("Employee Name ")
         self.code = int(input("Employee Code "))
         self.salary = float(input("Employee base salary "))
+        assert self.salary > 0, "Salary must be greater than 0"
         self.dependents = int(input("Number of Dependents "))
 
-class Paycheck:
-    def fgts(salary):
-        fgts = salary * 0.08
-    def inns(salary):
-        if salary <= 1412.00:
-            inss = salary * 0.075
-        elif salary >= 1412.01 and salary <= 2666.68:
-            inss = salary * 0.09
-        elif salary >= 2666.69 and salary <= 4000.03:
-            inss = salary * 0.12
+    def fgts(self):
+        return "%.2f" % (self.salary * 0.08)
+    
+    def inss(self):
+        if self.salary <= 1412.00:
+            return "%.2f" % (self.salary * 0.075)
+        elif self.salary >= 1412.01 and self.salary <= 2666.68:
+            return "%.2f" % (self.salary * 0.09)
+        elif self.salary >= 2666.69 and self.salary <= 4000.03:
+            return "%.2f" % (self.salary * 0.12)
         else:
-            inss = salary * 0.14
-    def ir(salary):
-        if salary <= 2112.00:
-            ir = 0
-        elif salary >= 2112.01 and salary <= 2826.65:
-            ir = salary * 0.075
-    def grossincome(self, salary):
-        self.extras = float(input("Employee monthly extras: "))
-        grossincome = salary + self.extras
-    def liquidincome(grossincome, ir, inss):
-        liquidincome = grossincome - ir - inss
-   
+            return "%.2f" % (self.salary * 0.14)
+        
+    def ir(self):
+        if self.salary <= 2112.00:
+            return 0
+        elif self.salary >= 2112.01 and self.salary <= 2826.65:
+            return "%.2f" % (self.salary * 0.075)
+        else:
+            return "%.2f" % (self.salary * 0.275)
+        
+    def grossIncome(self):
+        self.extras = 1700 #float(input("Employee monthly extras: "))
+        return "%.2f" % (self.salary + self.extras)
+    
+    # def liquidIncome(self):
+    #     return self.grossIncome - self.ir - self.inss
+        
+
+# class Paycheck(Employee):
+#     print(f"{Employee().name} ´s Paycheck")
+#     print(f"Employee code: {Employee().code}")
+#     print(f"Base salary: {Employee().salary}")
+#     print(f"Extras: {Employee().extras}")
+#     print(f"IR: {Employee().ir}")
+#     print(f"INSS: {Employee().inss}")
+#     print(f"Gross Income: {Employee().grossIncome}")
+#     print(f"Liquid Income: {Employee().liquidIncome}")
+
+employee1 = Employee()
+print(f"{employee1.name} ´s Paycheck")
+print(f"Employee code: {employee1.code}")
+print(f"Base salary: {employee1.salary}")
+# print(f"Extras: {employee1.extras}")
+print(f"IR: {employee1.ir()}")
+print(f"INSS: {employee1.inss()}")
+print(f"Gross Income: {employee1.grossIncome()}")
+# print(f"Liquid Income: {employee1.liquidIncome()}")
